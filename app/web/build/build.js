ed83681c32e392781297bf1d4cb305a487cf4b22
wuqingtao <qingtao.wu@live.cn>
2017-01-16 18:36:32 +0800

    更新build脚本

--- app/web/build/build.js
+++ app/web/build/build.js
@@ -1,29 +1,80 @@
 'use strict';
 
-var copy = require('copy');
-var fs = require("fs");
 var path = require("path");
+var fs = require("fs-extra");
+var str = require("string");
+var markdown = require('markdown-it');
 
-// 复制template/[css,img,js]/*至public
-copy('template/css/*', 'public/css', function() {});
-copy('template/img/*', 'public/img', function() {});
-copy('template/js/*', 'public/js', function() {});
+function copyTemplateRes() {
+	fs.copySync('template/css', 'public/css');
+	fs.copySync('template/img', 'public/img');
+	fs.copySync('template/js', 'public/js');
+}
 
-// 复制source/*/img/*至public
-var htmls = [];
-var filepaths = fs.readdirSync('template');
-for (var i in filepaths) {
-	var filepath = path.join('template', filepaths[i]);
-	var properties = path.parse(filepath);
-	if (properties.ext == '.html') {
-		htmls.push(properties.name);
+function copySourceRes(html) {
+	fs.copySync(path.join('source', html, 'img'), 'public/img');
+}
+
+function fetchTemplateHtmlFiles() {
+	var htmlFiles = [];
+	var dirs = fs.readdirSync('template');
+	for (var i in dirs) {
+		var properties = path.parse(path.join('template', dirs[i]));
+		if (properties.ext == '.html') {
+			htmlFiles.push(properties.name);
+		}
 	}
+	return htmlFiles;
 }
 
-for (var i in htmls) {
-	var html = htmls[i];
-	var filepath = path.join('source', html, 'img', '*');
-	copy(filepath, 'public/img', function() {});
+function fetchFileName(dir) {
+	var properties = path.parse(dir);
+	return htmlFiles.push(properties.name);
 }
 
-// MD文件渲染
+function fetchSourceMdFiles(html) {
+	var mdFiles = [];
+	var dirs = fs.readdirSync(path.join('source', html));
+	for (var i in dirs) {
+		var dir = path.join('source', html, dirs[i]);
+		var properties = path.parse(dir);
+		if (properties.ext == '.md') {
+			mdFiles.push(dir);
+		}
+	}
+	return mdFiles;
+}
+
+function parseMdFileToHeaders(mdFile) {
+	var headers = {};
+	var mdData = fs.readFileSync(mdFile, 'utf-8');
+	var tag = '---';
+	var lines = str(mdData).between(tag, tag).trim().lines();
+	for (var i in lines) {
+		var line = lines[i];
+		var cells = str(line).splitLeft(':', 1);
+		var key = cells[0].trim();
+		var val = cells[1].trim();
+		headers[key] = val;
+	}
+	return headers;
+}
+
+function parseMdFileToBody(mdFile) {
+	var mdData = fs.readFileSync(mdFile, 'utf-8');
+	var tag = '---';
+	var mdBody = mdData.substr(mdData.indexOf(tag) + tag.length);
+	var mdBody = mdBody.substr(mdBody.indexOf(tag) + tag.length);
+	var md = new markdown();
+	var body = md.render(mdBody);
+	return body;
+}
+
+function renderHtmlFile
+
+copyTemplateRes();
+copySourceRes('index');
+console.log(fetchTemplateHtmlName());
+console.log(fetchSourceMdFiles('index'));
+console.log(JSON.stringify(parseMdFileToHeaders(fetchSourceMdFiles('index')[0])));
+console.log(parseMdFileToBody(fetchSourceMdFiles('index')[0]));
